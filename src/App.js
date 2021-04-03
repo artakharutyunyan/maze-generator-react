784022ed6b3ad9fcc307f0e50e299c9e4b67abbb
Artak Harutyunyan <artak_harutyunyan@mail.com>
2021-04-03 18:21:55 +0400

    Adding row

--- src/App.js
+++ src/App.js
@@ -6,22 +6,68 @@ import "./style.css";
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      name: "React",
-    };
-  }
-
-  render() {
-    const walls = {
+    this.currentSetID = 1;
+    this.timesTicked = 0;
+    this.speed = 2000;
+    this.chanceToJoin = 0.3;
+    this.initialWalls = {
       left: true,
       right: true,
       top: true,
       bottom: true,
     };
+    this.state = {
+      width: 10,
+      height: 10,
+      cells: [],
+    };
+  }
+
+  componentDidMount() {
+    // this.timerID = setInterval(
+    //   () => this.tick(),
+    //   this.speed
+    // );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  createRow() {
+    this.setState({
+      cells: [
+        ...this.state.cells,
+        <Cell setID={this.currentSetID} walls={this.initialWalls} />,
+      ],
+    });
+    this.currentSetID += 1;
+  }
+
+  joinSomeCells() {
+    if (this.state.cells.length >= this.state.width) {
+      clearInterval(this.timerID);
+      return;
+    }
+    // this.setState
+  }
+
+  tick() {
+    this.timesTicked++;
+    console.log(this.timesTicked);
+    if (this.state.cells.length >= this.state.width) {
+      this.currentSetID = this.timesTicked - this.state.width;
+      this.joinSomeCells();
+      return;
+    }
+    this.createRow();
+  }
+
+  render() {
     return (
       <div>
-        <p>Maze Generation</p>
-        <Cell walls={walls} />
+        <p>Maze Generator</p>
+        {this.state.cells}
       </div>
     );
   }
--- src/Cell.js
+++ src/Cell.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Cell extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
+  /**
+   * extract the walls object into a string with from 'cell border-left ...' according to what borders need to be shown
+   */
   getWallsToShow(walls) {
     return Object.keys(walls).reduce((acc, currentWall) => {
       return `${acc} ${walls[currentWall] ? `border-${currentWall}` : ""}`;
@@ -14,11 +17,12 @@ class Cell extends Component {
 
   render() {
     const { walls } = this.props;
-    return <div className={this.getWallsToShow(walls)}></div>;
+    return <div className={this.getWallsToShow(walls)}>{this.props.setID}</div>;
   }
 }
 
 Cell.propTypes = {
+  setID: PropTypes.number,
   walls: PropTypes.exact({
     left: PropTypes.bool,
     right: PropTypes.bool,
